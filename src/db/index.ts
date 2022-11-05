2e5e65f387db100eb03b7915fe9f3fd0da8fea6f
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-11-05 18:39:36 +0500

    refactor(src/db): export prisma var

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,9 +1,11 @@
-import { PrismaClient } from '@prisma/client'
-const env = process.env
-export default new PrismaClient({
+import { PrismaClient } from "@prisma/client";
+const env = process.env;
+const prisma =  new PrismaClient({
     datasources: {
         db: {
-            url: env.NODE_ENV==="dev" ? env.LOCAL_DATABASE_URL : env.DATABASE_URL
-        }
-    }
-})
+            url: env.NODE_ENV === "dev" ? env.LOCAL_DATABASE_URL : env.DATABASE_URL,
+        },
+    },
+});
+
+export default prisma;
