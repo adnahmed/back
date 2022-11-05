3faa68cec588e7f10e2c79b958d25d496fff981e
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-11-05 13:14:00 +0500

    test dev

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', (req, res) => {
-  res.send({ hello: 'world' })
+  res.send({ hello: 'dev' })
 });
 app.use(function (req, res, next) {
   next(createError(404));
