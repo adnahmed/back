5420d1affda9e36dc048fad19973339486a6a375
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-11-06 13:18:57 +0500

    fix(src/app): use valid envs

--- src/app.ts
+++ src/app.ts
@@ -5,8 +5,13 @@ import createError from 'http-errors'
 import cookieParser from 'cookie-parser'
 import logger from 'morgan'
 import express from 'express'
-const env = process.env;
+import env from "../env"
+
 const app = express();
+const server = http.createServer(app);
+const port = normalizePort(env.PORT || '3000');
+
+app.set('port', port);
 app.enable("trust proxy"); // adjust for reverse proxy see: https://expressjs.com/en/guide/behind-proxies.html
 app.disable("x-powered-by");
 app.use(logger('dev'));
@@ -25,29 +30,14 @@ app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.send("Error")
 });
-const port = normalizePort(env.PORT || '3000');
-app.set('port', port);
-const server = http.createServer(app);
 server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
-
-function normalizePort(val) {
-  const port = parseInt(val, 10);
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
-}
 
 function onError(error) {
   if (error.syscall !== 'listen') {
     throw error;
   }
-
   const bind = typeof port === 'string'
     ? 'Pipe ' + port
     : 'Port ' + port;
@@ -64,10 +54,22 @@ function onError(error) {
       throw error;
   }
 }
+
 function onListening() {
   const addr = server.address();
   const bind = typeof addr === 'string'
     ? 'pipe ' + addr
     : 'port ' + addr?.port;
   console.log('Listening on ' + bind);
+}
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    return val;
+  }
+  if (port >= 0) {
+    return port;
+  }
+  return false;
 }
