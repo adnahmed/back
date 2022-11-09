95f5f040845be2607dd070c4d60d76524d795d09
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-11-09 11:08:49 +0500

    build(env.ts): add default values for redis env and make port required for de

--- src/env.ts
+++ src/env.ts
@@ -1,22 +1,21 @@
 import { cleanEnv, num, str } from "envalid";
 export default cleanEnv(process.env, {
-  APP_NAME: str(),
+  APP_NAME: str({default: "kesan-diary-api"}),
   APP_ENV: str({ choices: ["local", "dev", "test", "prod"] }),
   JWT_SECRET: str(),
   JWT_EXPIRES: num({ default: 60 * 60 * 24 * 14 /* 2 weeks */ }),
-  ...((process.env.NODE_ENV === "dev") && {
+  NODE_ENV: str({default: "dev"}),
+  ...((process.env.APP_ENV === "dev") && {
     LOCAL_DATABASE_URL: str(),
     REDIS_HOST: str(),
-    REDIS_PORT: num(),
-    REDIS_USERNAME: str(),
-    REDIS_PASSWORD: str(),
-    REDIS_DB: num(),
-    REDIS_MAX_CONNECTION_RETRY: num(),
-    REDIS_RECONNECT_TIME: num(),
-    NODE_ENV: str()
-  }),
-  ...(!(process.env.NODE_ENV === "dev") && {
-    DATABASE_URL: str(),
+    REDIS_PORT: num({default: 6379}),
+    REDIS_USERNAME: str({default: 'default'}),
+    REDIS_PASSWORD: str({default: ''}),
+    REDIS_MAX_CONNECTION_RETRY: num({default: 10}),
+    REDIS_RECONNECT_TIME: num({default: 3000}),
     PORT: num(),
+  }),
+  ...(!(process.env.APP_ENV === "dev") && {
+    DATABASE_URL: str(),
   }),
 });
